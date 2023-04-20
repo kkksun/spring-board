595540af357bf0ca73240af045c045077f9c3162
sunyoung <sun9011@gmail.com>
2023-04-20 18:35:33 +0900

    DTO, Entity 수정

--- src/main/java/project/springboard/domain/board/form/AddBoardForm.java
+++ src/main/java/project/springboard/domain/board/form/AddBoardForm.java
@@ -4,16 +4,18 @@ import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 import project.springboard.domain.board.entity.AttachFile;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class AddBoardForm {
-    private Long id;
 
     private Long userId;
 
+    @NotBlank
     private String title;
 
     private String content;
@@ -24,5 +26,4 @@ public class AddBoardForm {
 
     private List<MultipartFile> attachFileList;
 
-    private Boolean fileCheck;
 }
