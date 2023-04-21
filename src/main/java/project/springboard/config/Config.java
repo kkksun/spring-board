b8efeb853d6453d3d09b0269d241e7a84f2e0d8b
sunyoung <sun9011@gmail.com>
2023-04-21 10:12:39 +0900

    Session Interceptor 추가

--- src/main/java/project/springboard/config/Config.java
+++ src/main/java/project/springboard/config/Config.java
@@ -1,17 +1,11 @@
 package project.springboard.config;
 
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import project.springboard.config.filter.LoginSessionFilter;
 import project.springboard.config.interceptor.LoginSessionInterceptor;
 
-import javax.servlet.DispatcherType;
-import javax.servlet.Filter;
-import javax.servlet.FilterRegistration;
 
 @Configuration
 public class Config implements WebMvcConfigurer {
@@ -21,6 +15,6 @@ public class Config implements WebMvcConfigurer {
         registry.addInterceptor(new LoginSessionInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/","/css/**", "/error", "/login", "/join", "/logout", "/member/loginIdDuplicateCheck");
+                .excludePathPatterns("/","/index.html","/css/**", "/error", "/login", "/join", "/logout", "/member/loginIdDuplicateCheck");
     }
 }
