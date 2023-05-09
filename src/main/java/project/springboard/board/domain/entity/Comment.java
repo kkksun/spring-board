1b11252bb20cef656753b2b666e37bad08955804
sunyoung <sun9011@gmail.com>
2023-05-09 18:34:45 +0900

    comment 엔티티 생성

---
+++ src/main/java/project/springboard/board/domain/entity/Comment.java
@@ -0,0 +1,48 @@
+package project.springboard.board.domain.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import project.springboard.global.auditing.Auditable;
+import project.springboard.member.domain.entity.Member;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name ="COMMENTS")
+public class Comment extends Auditable {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "COMMENT_ID")
+     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "BOARD_ID")
+    private Board board;
+
+    private String comment;
+
+    @Column(name = "PARENTS_NUM")
+    private Long parentsNum;
+
+    @Column(name = "GROUP_NUM")
+    private Long groupNum;
+
+    private Long level;
+
+    private Long order;
+
+    @Column(name = "DEL_YN")
+    private Check delCheck;
+
+
+}
