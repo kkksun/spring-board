5cdc0fd114ca3be1dcab40a148187465e0fcfc4d
Sunyoung <sun9011@gmail.com>
2023-05-19 08:29:40 +0900

    image 추가

--- src/main/resources/static/js/board/function/createCommentListHtml.js
+++ src/main/resources/static/js/board/function/createCommentListHtml.js
@@ -14,8 +14,11 @@ const createCommentListHtml = (isFirst, data) => {
         const commentMargin = d["parentId"] != null ? margin : "0px";
         const addClass = d["parentId"] != null ?  `pre${d["parentId"]}` : "" ;
         html += `<div clss= "comment ${addClass}" id = "comment${d["id"]}">
-                      <div class="commentArea" style="padding-left: ${commentPadding}px">
-                            <img class="userIcon" src="/images/userIcon.png" >
+                      <div class="commentArea" style="padding-left: ${commentPadding}px">`
+        if(d["parentId"] != null) {
+            html += `        <img class="arrowIcon" src="/images/arrowIcon.png" >`;
+        }
+         html += `           <img class="userIcon" src="/images/userIcon.png" >
                             <div class="commentBox">
                                 <div class="commentBox_loginId">${d["loginId"]}</div>
                                 <div class="commentBox_comment">
