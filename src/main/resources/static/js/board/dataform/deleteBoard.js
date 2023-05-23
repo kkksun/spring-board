156e29e9b6cb5191a483e36fd93de62e2b7a0bd3
sunyoung <sun9011@gmail.com>
2023-05-23 13:20:58 +0900

    조건절 수정

--- src/main/resources/static/js/board/dataform/deleteBoard.js
+++ src/main/resources/static/js/board/dataform/deleteBoard.js
@@ -2,18 +2,19 @@ const deleteBoard = (data) => {
     const commentCnt = data.length;
     if(memberIdOfBoard === idOfLoginMember && commentCnt != 0) {
         alert("게시글 삭제가 불가합니다. 관리자에게 문의해 주세요.");
-    }
-    if((memberIdOfBoard === idOfLoginMember && commentCnt == 0)|| typeOfLoginMember != "USER") {
-        if(confirm("삭제하시겠습니까?")) {
-            fetch("/api/board/delete/" + boardId, {
-                method : "DELETE"
-            }).then(response => {
-                if (response.ok) {
-                    location.href = document.location.origin + "/complete/board?type=DELETE";
-                } else {
-                    throw new Error(response.status + " 오류가 발생하였습니다.")
-                }
-            }).catch(err => alert(err))
+    } else {
+        if ((memberIdOfBoard === idOfLoginMember && commentCnt == 0) || typeOfLoginMember != "USER") {
+            if (confirm("삭제하시겠습니까?")) {
+                fetch("/api/board/delete/" + boardId, {
+                    method: "DELETE"
+                }).then(response => {
+                    if (response.ok) {
+                        location.href = document.location.origin + "/complete/board?type=DELETE";
+                    } else {
+                        throw new Error(response.status + " 오류가 발생하였습니다.")
+                    }
+                }).catch(err => alert(err))
+            }
         }
     }
 }
